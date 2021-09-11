594ecdec74933d874a6531d41d885ee8c7811fa2
pejman <pejman.hkh@gmail.com>
2021-09-11 20:25:21 +0430

    e

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # otcc-as-jit-compiler
 Fabrice Bellard Otcc Project as Jit Compiler
+
+
+# Compile
+
+`
+gcc -m32 otccn.c -o otccn -ldl
+`
+
+# Run
+
+`
+./otccn hello.c
+`
---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+test() {
+	printf("test\n");
+}
+
+main() {
+	int a, b, c; 
+	a = 3*5+1;
+	b = 24/3+1;
+	c = a+b;
+	printf("%d\n", c );
+	printf("hello world\n");
+	test();
+
+	return 0;
+}
+
